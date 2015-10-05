f042bc47a5b40c0d74f3a272e3344d8068ce51b3
babassmine <bassco10@gmail.com>
2015-10-04 20:07:48 -0400

    Finish log in/log out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
-  
-	def show 
+
+	def show
 		@user = User.find(params[:id])
 		#debugger
 	end
@@ -9,9 +9,10 @@ class UsersController < ApplicationController
 		@user = User.new
   end
 
-	def create 
+	def create
 		@user = User.new(user_params)
 		if @user.save
+			log_in @user
 			flash[:success] = "Welcome to the Sample App!"
 			redirect_to @user
 		else
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,9 +1,9 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  
+
 	def setup
-		@user = User.new(name: "Example User", email: "user@example.com", 
+		@user = User.new(name: "Example User", email: "user@example.com",
 										 password: "foobar", password_confirmation: "foobar")
 	end
 
@@ -15,7 +15,7 @@ class UserTest < ActiveSupport::TestCase
     @user.name = "     "
     assert_not @user.valid?
   end
-	
+
 	test "email should be present" do
 		@user.email = "      "
 		assert_not @user.valid?
@@ -30,7 +30,7 @@ class UserTest < ActiveSupport::TestCase
     @user.email = "a" * 244 + "@example.com"
     assert_not @user.valid?
   end
-	
+
 	test "email validation should accept valid addresses" do
 		valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
 		                         first.last@foo.jp alice+bob@baz.cn]
@@ -55,7 +55,7 @@ class UserTest < ActiveSupport::TestCase
 		@user.save
 		assert_not duplicate_user.valid?
 	end
-	
+
 	test "password should be present (nonblank)" do
 		@user.password = @user.password_confirmation = "a" * 5
 		assert_not @user.valid?
@@ -71,5 +71,9 @@ class UserTest < ActiveSupport::TestCase
     @user.email = mixed_case_email
     @user.save
     assert_equal mixed_case_email.downcase, @user.reload.email
+  end
+
+  test "authenticated? should return false for a user with nil digest" do
+    assert_not @user.authenticated?('')
   end
 end
